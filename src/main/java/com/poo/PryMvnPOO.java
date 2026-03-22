f202ba3cb4fc65994cfd31f11b98f2886aa88b5b
Oscar Alberto Barnett Contreras <54248555+obarnettc@users.noreply.github.com>
2026-03-22 17:16:16 -0500

    Add current date output to console app

--- src/main/java/com/poo/PryMvnPOO.java
+++ src/main/java/com/poo/PryMvnPOO.java
@@ -1,7 +1,11 @@
 package com.poo;
+
+import java.time.LocalDate;
+
 public class PryMvnPOO {
     public static void main(String[] args) {
         System.out.println("Desde Github");
         System.out.println("Hola UTP");
+        System.out.println("Fecha actual: " + LocalDate.now());
     }
 }
